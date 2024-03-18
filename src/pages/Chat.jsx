551ad09ad339609433f8919f612f41f3488a3c47
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-03-18 14:15:26 +0600

    chat add

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-
+import { IoMdMore } from "react-icons/io";
+import { FaSearch } from "react-icons/fa";
+import Parson from "./Parson";
 
 function Chat() {
-  return <div></div>;
+  return (
+    <div className=" w-1/3 p-4 rounded-lg bg-white mt-4">
+      <div className=" flex justify-between ">
+        <h2 className="title font-semibold text-2xl">Chat</h2>
+        <button>
+          <IoMdMore />
+        </button>
+      </div>
+      <div className=" flex w-full items-center py-2 px-3 gap-3 text-slate-600 border-0 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500  transition ease-in-out duration-150 mt-4">
+        <FaSearch />
+        <input className=" w-full h-8 pl-3" type="text " placeholder="Search" />
+      </div>
+      <div className=" flex flex-col gap-3 mt-4">
+      <Parson/>
+      <Parson/>
+      <Parson/>
+      <Parson/>
+      <Parson/>
+      <Parson/>
+      </div>
+    </div>
+  );
 }
 
 export default Chat;
